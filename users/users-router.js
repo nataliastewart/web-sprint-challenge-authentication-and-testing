48c3d83b5cce88660f3eeb92d546a4f96e75dc81
Natalia <nataliamirandac@hotmail.com>
2020-06-19 12:41:47 -0400

    Logged and GOT the JOKES - with the Authorization header and TOKEN

--- users/users-router.js
+++ users/users-router.js
@@ -3,22 +3,12 @@ const router = require("express").Router();
 const Users = require("./users-model");
 const restricted = require("../auth/authenticate-middleware");
 
-router.get("/", restricted, checkDepart(null), (req, res) => {
+router.get("/", restricted, (req, res) => {
   Users.find()
     .then((users) => {
       res.status(200).json({ users, decodedToken: req.decodedToken });
     })
     .catch((err) => res.send(err));
 });
-
-function checkDepart(department) {
-  return (req, res, next) => {
-    if (req.decodedToken.department === department) {
-      next();
-    } else {
-      res.status(403).json({ message: "you have no power here" });
-    }
-  };
-}
 
 module.exports = router;
